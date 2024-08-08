47452f1798db4517374d2931b5b260047122dbb8
Alexandre Brunet <alexandrebrunet@Alexandres-MacBook-Pro-2.local>
2024-08-08 10:45:16 -0400

    WIP: adding data files and testing views on map

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,12 +1,12 @@
 import pytest
 import mimetypes
-from src.utils import read_file
+from src.utils import file_type
 
 def test_read_txt_success(tmpdir):
     file_path = tmpdir.join("test.txt")
     file_path.write("Hello, World!")
     
-    result = read_file(str(file_path))
+    result = file_type(str(file_path))
     assert result["content"] == b"Hello, World!"
     assert result["file_type"] == "text/plain"
     assert result["encoding"] is None
@@ -34,11 +34,11 @@ def test_read_geojson_success(tmpdir):
         ]
     }
     '''
-    
+
     file_path = tmpdir.join("test.geojson")
     file_path.write(geojson_content)
     
-    result = read_file(str(file_path))
+    result = file_type(str(file_path))
     assert result["content"] == geojson_content.encode()
     assert result["file_type"] == "application/geo+json"
     assert result["encoding"] is None
@@ -46,7 +46,7 @@ def test_read_geojson_success(tmpdir):
 
 def test_read_file_not_found():
     # Appeler la fonction read_file avec un chemin de fichier inexistant
-    result = read_file("non_existent_file.txt")
+    result = file_type("non_existent_file.txt")
     
     # Vérifier le message d'erreur
     assert result["error"] == "File not found"
