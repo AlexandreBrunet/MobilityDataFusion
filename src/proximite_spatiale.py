1fe414adb7ff5587464a90a05dba874485348de4
Alexandre Brunet <alexandrebrunet@Alexandres-MacBook-Pro-2.local>
2024-08-08 10:45:16 -0400

    WIP: adding data files and testing views on map

--- src/proximite_spatiale.py
+++ src/proximite_spatiale.py
@@ -1,10 +1,43 @@
 import pandas as pd
-import geopandas as gdp
-from utils import file_type, files_to_df, check_geometry_column
+import geopandas as gpd
+import pydeck as pdk
 
-file_path = './data/lieux-fr.geojson'
-file_type = file_type(file_path=file_path)
+# Lire le fichier GeoJSON
+file_path = './data/test.geojson'
+gdf = gpd.read_file(file_path)
 
-data = files_to_df(file_path, file_type)
-print(data.head(10))
-check_if_geo = check_geometry_column(data)
+# Convertir les données GeoPandas en DataFrame pandas
+df = pd.DataFrame(gdf.drop(columns='geometry'))
+df['lon'] = gdf.centroid.x
+df['lat'] = gdf.centroid.y
+
+# Vérifier les données après conversion
+print(df.head())
+
+# Créer la couche Pydeck pour les points
+scatter_layer = pdk.Layer(
+    'ScatterplotLayer',
+    data=df,
+    get_position='[lon, lat]',
+    get_fill_color='[200, 30, 0, 160]',
+    get_radius=100,
+    pickable=True
+)
+
+# Créer la vue initiale de la carte
+view_state = pdk.ViewState(
+    latitude=df['lat'].mean(),
+    longitude=df['lon'].mean(),
+    zoom=10,
+    pitch=0,
+)
+
+# Créer la carte Pydeck
+r = pdk.Deck(
+    layers=[scatter_layer],
+    initial_view_state=view_state,
+    map_style='dark'
+)
+
+# Afficher la carte
+r.to_html('map.html', open_browser=True)
