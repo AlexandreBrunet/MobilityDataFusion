65998877576b3ddf22de11022a44c36f2b0c4aac
Alexandre Brunet <alexandrebrunet@AlexandresMBP2.hitronhub.home>
2024-08-12 13:18:31 -0400

    WIP code to add point and buffer around

--- src/proximite_spatiale.py
+++ src/proximite_spatiale.py
@@ -4,83 +4,62 @@ import pydeck as pdk
 import utils as utils
 
 file_path = './data/stations_bixi.geojson'
-# file_path = './data/evaluation_fonciere_test.geojson'
+
 gdf = gpd.read_file(file_path)
 
 geometry_column = utils.check_geometry_column(gdf)
-print(geometry_column)
 
 check_geom_type = utils.check_geometry_type(gdf)
 
-if check_geom_type == 'Point':
-    point_layer = pdk.Layer(
-        'ScatterplotLayer',
-        data=gdf,
-        get_position='[geometry.coordinates[0], geometry.coordinates[1]]',
-        get_color='[200, 30, 0, 160]',
-        get_radius=10,
-        pickable=True
-    )
-    
-    layers = [point_layer]
-else:
-    raise ValueError("Type de géométrie non supporté.")
+# Créer un buffer de 100 mètres autour de chaque point
+gdf['buffer'] = gdf.geometry.buffer(0.001)
 
+# Convertir le GeoDataFrame en DataFrame compatible avec pydeck
+gdf = gdf.to_crs(epsg=4326)  # S'assurer que les coordonnées sont en lat/lon
+gdf['lon'] = gdf.geometry.x
+gdf['lat'] = gdf.geometry.y
 
-# Configurer la vue
+# Extraire les coordonnées du buffer sous forme de liste de listes
+gdf['buffer_coordinates'] = gdf['buffer'].apply(lambda geom: [[x, y] for x, y in zip(geom.exterior.coords.xy[0], geom.exterior.coords.xy[1])])
+print(gdf.head(10))
+
+# Sélectionner les colonnes nécessaires pour éviter d'avoir des objets complexes
+df = gdf[['name', 'capacity', 'lon', 'lat', 'buffer_coordinates']].copy()
+
+# Création de la couche pour les points
+point_layer = pdk.Layer(
+    'ScatterplotLayer',
+    data=df,
+    get_position='[lon, lat]',
+    get_color='[200, 30, 0, 160]',
+    get_radius=10,
+    pickable=True
+)
+
+# Création de la couche pour les buffers
+buffer_layer = pdk.Layer(
+    'PolygonLayer',
+    data=df,
+    get_polygon='buffer_coordinates',
+    get_fill_color='[0, 0, 200, 50]',
+    get_line_color='[0, 0, 200, 200]',
+    pickable=True
+)
+
+# Définition de la vue initiale
 view_state = pdk.ViewState(
-    latitude=gdf.geometry.centroid.y.mean(),
-    longitude=gdf.geometry.centroid.x.mean(),
-    zoom=50,
+    latitude=df['lat'].mean(),
+    longitude=df['lon'].mean(),
+    zoom=14,
     pitch=0,
 )
 
-# Créer la carte
+# Création de la carte avec les deux couches
 r = pdk.Deck(
-    layers=layers,
+    layers=[point_layer, buffer_layer],
     initial_view_state=view_state,
     map_style='dark'
 )
 
-# Sauvegarder la carte en HTML
+# Exporter la carte en HTML et l'ouvrir dans le navigateur
 r.to_html('map.html', open_browser=True)
-
-
-
-
-
-
-# if gdf.geom_type.unique().size == 1:
-#     geom_type = gdf.geom_type.iloc[0]
-# else:
-#     raise ValueError("Les données contiennent plusieurs types de géométrie.")
-
-# if geom_type == 'Polygon' or geom_type == 'MultiPolygon':
-#     gdf['coordinates'] = gdf['geometry'].apply(lambda x: list(x.exterior.coords) if x.geom_type == 'Polygon' else list(x.representative_point().coords))
-    
-#     polygon_layer = pdk.Layer(
-#         'PolygonLayer',
-#         data=gdf,
-#         get_polygon='coordinates',
-#         get_fill_color='[200, 30, 0, 160]',
-#         get_line_color='[255, 255, 255]',
-#         get_line_width=2,
-#         pickable=True
-#     )
-    
-#     layers = [polygon_layer]
-    
-# elif geom_type == 'Point':
-#     # Créer un layer pour les points
-#     point_layer = pdk.Layer(
-#         'ScatterplotLayer',
-#         data=gdf,
-#         get_position='[geometry.coordinates[0], geometry.coordinates[1]]',
-#         get_color='[200, 30, 0, 160]',
-#         get_radius=100,
-#         pickable=True
-#     )
-    
-#     layers = [point_layer]
-# else:
-#     raise ValueError("Type de géométrie non supporté.")
