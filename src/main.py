be78096a2ee35888e88bfb577ff82b8d6541fd31
Alexandre Brunet <alexandrebrunet@Alexandres-MacBook-Pro-2.local>
2024-09-12 13:38:11 -0400

    refactoring buffer gdf

--- src/main.py
+++ src/main.py
@@ -17,9 +17,8 @@ first_gdf = utils.add_lon_lat_columns(first_gdf)
 second_gdf = utils.add_lon_lat_columns(second_gdf)
 
 buffer_gdf = buffer.buffer_oiseau(second_gdf, 'buffer', 0.001)
-buffer_gdf = buffer_gdf.to_crs(epsg=4326)
-buffer_gdf['lon'] = buffer_gdf.geometry.x
-buffer_gdf['lat'] = buffer_gdf.geometry.y
+buffer_gdf = utils.check_and_correct_crs(buffer_gdf)
+buffer_gdf = utils.add_lon_lat_columns(buffer_gdf)
 buffer_gdf['buffer_coordinates'] = buffer_gdf['buffer'].apply(lambda geom: [[x, y] for x, y in zip(geom.exterior.coords.xy[0], geom.exterior.coords.xy[1])])
 
 second_gdf = second_gdf.drop(columns=['buffer'])
@@ -34,15 +33,3 @@ first_layer = visualisation.create_point_layer(first_gdf, rouge)
 second_layer = visualisation.create_point_layer(second_gdf, vert)
 buffer_layer = visualisation.create_polygon_layer(buffer_gdf)
 visualisation.create_map_layers([first_layer, second_layer, buffer_layer], initial_view)
-
-
-# print(gdf.head(10))
-# print(len(gdf))
-# print(second_df.head(10))
-# print(len(second_df))
-
-# joined_gdf = gpd.sjoin(gdf.set_geometry('buffer'), second_df, how="inner", predicate='intersects')
-# print(joined_gdf.head(10))
-# print(len(joined_gdf))
-
-# joined_gdf.to_excel('test.xlsx', index=False)
