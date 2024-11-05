15f76d002d8875817becfad65563d3080737ed34
Alexandre Brunet <alexandrebrunet@Alexandres-MacBook-Pro-2.local>
2024-11-05 09:21:14 -0500

    moving codes in main for clarity

--- src/main.py
+++ src/main.py
@@ -38,20 +38,20 @@ for layer_name, gdf in geodataframes:
     # Remplir les valeurs manquantes par 0
     gdf = gdf.fillna(0)
 
-    # Séparer points et polygones
-
+    # Créer les gdf pour les points, les polygones et le buffer
     points_gdf = gdfExtraction.extract_points_gdf(gdf)
-    points_layer = visualisation.create_point_layer(points_gdf, colors[layer_name])
 
     polygons_gdf = gdfExtraction.extract_polygons_gdf(gdf)
+    
+    buffer_gdf = buffer.apply_buffer(points_gdf, layer_name, buffer_layers)
+    buffer_gdf = gdfExtraction.extract_poly_coordinates(buffer_gdf)
+
+    points_layer = visualisation.create_point_layer(points_gdf, colors[layer_name])
     polygons_layer = visualisation.create_polygon_layer(polygons_gdf, colors[layer_name])
+    buffer_layer = visualisation.create_polygon_layer(buffer_gdf, '[128, 0, 128, 200]')
 
     layers.append(points_layer)
     layers.append(polygons_layer)
-
-    buffer_gdf = buffer.apply_buffer(points_gdf, layer_name, buffer_layers)
-    buffer_gdf = gdfExtraction.extract_poly_coordinates(buffer_gdf)
-    buffer_layer = visualisation.create_polygon_layer(buffer_gdf, '[200, 100, 50, 60]')
     layers.append(buffer_layer)
 
 
@@ -60,5 +60,3 @@ initial_view = visualisation.create_initial_view()
 
 # Créer la carte avec les couches de points, polygones et buffers
 visualisation.create_map_layers(layers, initial_view)
-
-##les gens vont vouloir un gros ficher avec toutes les colonnes
