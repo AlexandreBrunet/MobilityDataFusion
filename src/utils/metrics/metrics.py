1036b57ae657ad446000885407ee96bd47e68677
Alexandre Brunet <alexandrebrunet@Alexandres-MacBook-Pro-2.local>
2024-11-15 09:27:31 -0500

    adding std metrics

--- src/utils/metrics/metrics.py
+++ src/utils/metrics/metrics.py
@@ -1,7 +1,7 @@
 
 def aggregate_stats(agg_fusion_gdf,groupby_columns, agg_columns, count_columns):
     # Créer un dictionnaire pour les opérations d'agrégation
-    agg_dict = {col: ['min', 'max', 'mean'] for col in agg_columns}
+    agg_dict = {col: ['min', 'max', 'mean', 'std'] for col in agg_columns}
     agg_dict.update({col: 'count' for col in count_columns})
     
     # Effectuer l'agrégation
