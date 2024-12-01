726b91ca73c5f5a337734023d90bd4a5e0fceb00
Alexandre Brunet <alexandrebrunet@AlexandresMBP2.hitronhub.home>
2024-12-01 14:25:42 -0500

    changing name function

--- src/utils/buffer/buffer.py
+++ src/utils/buffer/buffer.py
@@ -2,18 +2,7 @@ import geopandas as gpd
 import itertools
 import utils.buffer.buffer as buffer
 
-def apply_buffer(points_gdf: gpd.GeoDataFrame, layer_name: str, buffer_layers: dict) -> gpd.GeoDataFrame:
-    """
-    Applique un buffer aux géométries de type 'Point' dans un GeoDataFrame pour une couche spécifique.
-    
-    Paramètres :
-    - points_gdf : GeoDataFrame contenant des points.
-    - layer_name : nom de la couche pour laquelle appliquer le buffer.
-    - buffer_layers : dictionnaire où les clés sont les noms de couche et les valeurs sont les distances de buffer en mètres.
-    
-    Retourne :
-    - buffer_gdf : GeoDataFrame avec le buffer appliqué si la couche correspond.
-    """
+def apply_points_buffer(points_gdf: gpd.GeoDataFrame, layer_name: str, buffer_layers: dict) -> gpd.GeoDataFrame:
     buffer_gdf = points_gdf.copy()
     
     # Vérifie si la couche correspond à une entrée dans le dictionnaire de buffers
@@ -32,7 +21,7 @@ def apply_buffer(points_gdf: gpd.GeoDataFrame, layer_name: str, buffer_layers: d
 def create_buffers(points_gdfs, buffer_layers):
     unique_id_counter = itertools.count(1)
     buffer_gdfs = {
-        f"{layer_name}_buffer": buffer.apply_buffer(points_gdfs[layer_name], layer_name, buffer_layers)
+        f"{layer_name}_buffer": buffer.apply_points_buffer(points_gdfs[layer_name], layer_name, buffer_layers)
                                        .assign(layer_name=f"{layer_name}_buffer",
                                                buffer_id=lambda df: [next(unique_id_counter) for _ in range(len(df))])
         for layer_name in buffer_layers
