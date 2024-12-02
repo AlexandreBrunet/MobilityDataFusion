c3968ce2dd8776495fd1acfb1b4c290eb3d74089
Alexandre Brunet <alexandrebrunet@AlexandresMBP2.hitronhub.home>
2024-12-02 16:39:54 -0500

    changing joins to add layer_name geometry

--- src/utils/gdf/joins.py
+++ src/utils/gdf/joins.py
@@ -14,16 +14,26 @@ def get_join_layers(points_gdfs, polygons_gdfs, multipolygons_gdfs, linestrings_
         join_data["linestrings"] = linestrings_gdfs
     return join_data
 
-# Appliquer les jointures sans hardcoding
 def perform_spatial_joins(buffer_gdfs, join_data, join_layers):
     buffer_joins = []
     for layer_name, buffer_gdf in buffer_gdfs.items():
         for geom_type, gdfs in join_data.items():
             join_type = join_layers[geom_type]["type"]
             for join_layer_name, join_gdf in gdfs.items():
-                join_result = gpd.sjoin(buffer_gdf, join_gdf, how="inner", predicate=join_type).assign(
+                # Effectuer la jointure spatiale
+                join_result = gpd.sjoin(buffer_gdf, join_gdf, how="inner", predicate=join_type)
+                
+                # Ajouter les géométries originales de la couche de jointure
+                geometry_column_name = f"{join_layer_name}_geometry"  # Nommage de la colonne pour la géométrie
+                join_result[geometry_column_name] = join_gdf.geometry
+                
+                # Ajouter des informations sur la couche de buffer et le type de jointure
+                join_result = join_result.assign(
                     buffer_layer=layer_name,
                     join_type=join_layer_name
                 )
+                
                 buffer_joins.append(join_result)
+    
+    # Combiner tous les résultats de jointure en un seul DataFrame
     return pd.concat(buffer_joins, ignore_index=True)
